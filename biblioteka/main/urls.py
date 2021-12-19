d713fb07a6ea11f9c0093a1c50964e699a71193c
arvsites@gmail.com <arvsites@gmail.com>
2021-12-19 22:46:43 +0500

    v1.3

--- biblioteka/main/urls.py
+++ biblioteka/main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name = ''),
     path('about', views.about, name = 'about'),
     path('readers', views.readers, name = 'readers'),
     path('search', views.search, name = 'search'),
